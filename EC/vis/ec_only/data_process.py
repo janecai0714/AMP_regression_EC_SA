969d19dfa9a676adf88bab63c3699313a01bad0e
janecai0714 <63832501+janecai0714@users.noreply.github.com>
2024-03-20 14:27:16 +0800

    Delete EC directory

--- EC/vis/ec_only/data_process.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-ec = pd.read_csv("/home/jianxiu/Documents/EC/vis/ec_only/EC.csv")
-sa = pd.read_csv("/home/jianxiu/Documents/EC/vis/ec_only/SA.csv")
-
-ec_only = ec.loc[pd.merge(ec, sa, on=['ID','SEQUENCE'], how='left', indicator=True)['_merge'] == 'left_only']
-sa_only = sa.loc[pd.merge(sa, ec, on=['ID','SEQUENCE'], how='left', indicator=True)['_merge'] == 'left_only']
-print("smart")
